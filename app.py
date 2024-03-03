63a67610a78b09d77640951846758422e05f939b
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-03-02 21:16:09 -0600

    Agregadas tablas a vista web

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def main() -> None:
                     if next_element[1] == "O":
                         temp_row[2] = None
 
-    print_tables(objects_table, semantic_network)
+    # print_tables(objects_table, semantic_network)
     display_graph(objects_table, semantic_network)
 
 
@@ -131,7 +131,13 @@ def display_graph(
 
     mermaid_code = generate_mermaid(objects_table, semantic_network)
     web_window = webui.window()
-    web_window.show(template.render(mermaid_code=mermaid_code))
+    web_window.show(
+        template.render(
+            mermaid_code=mermaid_code,
+            objects_table=objects_table,
+            semantic_network=semantic_network,
+        ),
+    )
     webui.wait()
 
 
