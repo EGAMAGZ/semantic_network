68d7bf0d5663ad896c75182c2ddeb9187ccbf6dd
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-03-04 00:53:51 -0600

    Terminado funcionamiento basico de red semantica

--- app.py
+++ app.py
@@ -1,99 +1,58 @@
-import re
 from webui import webui
-from enum import Enum
-from typing import Literal
 from jinja2 import Environment, FileSystemLoader
 from python_mermaid.diagram import Node, Link, MermaidDiagram
+from util.text import TextInfo, divide_text
 
 type SemanticNetwork = dict[int, tuple[int, int, int]]
-type ObjectsTable = dict[int, tuple[str, Literal["R", "O"]]]
-
-
-class ObjectType(Enum):
-    OBJECT = "O"
-    RELATION = "R"
-    NONE = "N"
+type ObjectsTable = dict[int, TextInfo]
 
 
 def main() -> None:
-    with open("entrada.txt") as file:
+    with open("data/info.txt") as file:
         content: str = file.read().replace("\n", "")
     sentences: list[str] = content.split(".")
 
     objects_table: ObjectsTable = {}
-
     total_objects: int = 0
-
-    current_value: str = ""
-    current_object_type: ObjectType = ObjectType.NONE
-
-    for sentence in sentences:
-        clean_sentence = sentence.strip()
-
-        for character in clean_sentence:
-            if character == "[":
-                current_object_type = ObjectType.RELATION
-
-            if character == "(":
-                current_object_type = ObjectType.OBJECT
-
-            if current_object_type != ObjectType.NONE:
-                current_value += character
-
-            if character == "]" or character == ")":
-                current_row = (
-                    re.sub(r"[\[\]\(\)]", "", current_value),
-                    current_object_type.value,
-                )
-                if current_row not in objects_table.values():
-                    total_objects += 1
-                    objects_table[total_objects] = current_row
-
-                current_object_type = ObjectType.NONE
-                current_value = ""
 
     number_row = 0
     semantic_network: SemanticNetwork = {}
 
-    keys = list(objects_table.keys())
-    temp_row: list[int | None] = [None, None, None]
+    words_index: dict[str, int] = {}
 
-    for key, (_, object_type) in objects_table.items():
-        current_index = keys.index(key)
+    for sentence in sentences:
+        if not sentence:
+            continue
 
-        if object_type == "O":
-            if temp_row[0] is None:
-                temp_row[0] = key
-            else:
-                if temp_row[2] is None:
-                    temp_row[2] = key
+        clean_sentence = sentence.strip()
 
-        if object_type == "R" and temp_row[1] is None:
-            temp_row[1] = key
+        group_1, group_2, group_3 = divide_text(clean_sentence)
 
-        if (
-            temp_row[0] is not None
-            and temp_row[1] is not None
-            and temp_row[2] is not None
-        ):
-            number_row += 1
-            semantic_network[number_row] = tuple(temp_row)
+        for object_1 in group_1:
+            for object_2 in group_2:
+                for object_3 in group_3:
 
-            if current_index < len(keys) - 1:
-                next_key = keys[current_index + 1]
-                next_element = objects_table.get(next_key)
+                    if object_1 not in objects_table.values():
+                        total_objects += 1
+                        objects_table[total_objects] = object_1
+                        words_index[object_1] = total_objects
 
-                if object_type == "R":
-                    if next_element[1] == "O":
-                        temp_row[2] = None
+                    if object_2 not in objects_table.values():
+                        total_objects += 1
+                        objects_table[total_objects] = object_2
+                        words_index[object_2] = total_objects
 
-                if object_type == "O":
-                    if next_element[1] == "R":
-                        temp_row[1] = None
-                        temp_row[2] = None
+                    if object_3 not in objects_table.values():
+                        total_objects += 1
+                        objects_table[total_objects] = object_3
+                        words_index[object_3] = total_objects
 
-                    if next_element[1] == "O":
-                        temp_row[2] = None
+                    semantic_network[number_row] = (
+                        words_index[object_1],
+                        words_index[object_2],
+                        words_index[object_3],
+                    )
+                    number_row += 1
 
     display_graph(objects_table, semantic_network)
 
@@ -107,6 +66,7 @@ def generate_mermaid(
         for key, (text, object_type) in objects_table.items()
         if object_type == "O"
     }
+
     edges = {
         key: text
         for key, (text, object_type) in objects_table.items()
---
+++ util/text.py
@@ -0,0 +1,55 @@
+import re
+from typing import Literal
+
+type TextInfo = tuple[str, Literal["R", "O"]]
+
+
+def divide_text(sentence: str):
+
+    pattern = r"(?P<grupo1>\(.*?\))\s*(?=\[)(?P<grupo2>\[.*?\])\s*(?P<grupo3>\(.*?\))"
+
+    coincidences = re.search(pattern, sentence)
+
+    group_1: list[TextInfo] = list(
+        map(
+            lambda text: (re.sub(r"[\(\)]", "", text), "O"),
+            re.findall(r"\(.*?\)", coincidences.group("grupo1")),
+        )
+    )
+    group_2: list[TextInfo] = list(
+        map(
+            lambda text: (re.sub(r"[\[\]]", "", text), "R"),
+            re.findall(r"\[.*?\]", coincidences.group("grupo2")),
+        )
+    )
+    group_3: list[TextInfo] = list(
+        map(
+            lambda text: (re.sub(r"[\(\)]", "", text), "O"),
+            re.findall(
+                r"\(.*?\)",
+                coincidences.group("grupo3") + sentence[coincidences.end() :],
+            ),
+        )
+    )
+
+    return [group_1, group_2, group_3]
+
+
+if __name__ == "__main__":
+    text = "(el agua) y (el jabon) [se usan en] y [se encuentran en] (la casa), (la escuela) y (la oficina)."
+    groups = divide_text(text)
+
+    assert groups == [
+        [("(el agua)", "O"), ("(el jabon)", "O")],
+        [("[se usan en]", "R"), ("[se encuentran en]", "R")],
+        [("(la casa)", "O"), ("(la escuela)", "O"), ("(la oficina)", "O")],
+    ]
+
+    text = "(Gamaliel) [juegan] y [se divierte] (la casa)"
+    groups = divide_text(text)
+
+    assert groups == [
+        [("(Gamaliel)", "O")],
+        [("[juegan]", "R"), ("[se divierte]", "R")],
+        [("(la casa)", "O")],
+    ]
